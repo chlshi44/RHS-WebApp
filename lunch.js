d94b055a1f302259759480070533509dcaa0684e
chlshi44 <chlshi44@roosevelths.k12.hi.us>
2016-02-29 09:21:44 -1000

    Update lunch.js

--- lunch.js
+++ lunch.js
@@ -1,52 +1,91 @@
-//Now input days of the month into a single array. Then set the days to match the lunch//
-var lunch = {
-1: "Cheeseburger",
-2: "Taco",
-3: "Saimin",
-4: "Steak",
-5: "Udon",
-6: "Katsu",
-7: "Kim Chee",
-8: "Pizza",
-9: "Egg Roll",
-10: "Food1",
-11: "Food2",
-12: "Food3",
-13: "Food4",
-14: "Food5",
-15: "Food6",
-16: "Food7",
-17: "Food8",
-18: "Food9",
-19: "Food10",
-20: "FoodA",
-21: "FoodB",
-22: "FoodC",
-23: "FoodD",
-24: "FoodE",
-25: "FoodF",
-26: "FoodG",
-27: "FoodH",
-28: "FoodI",
-29: "FoodJ",
-30: "FoodK",
-31: "FoodL"
+var DaysInMonth = {
+		0: 31,
+    1: 29,
+    2: 31,
+    3: 30,
+    4: 31,
+    5: 30,
+    6: 31,
+    7: 31,
+    8: 30,
+    9: 31,
+    10: 30,
+    11: 31
+}
+
+var bSchedule = {
+1: "Normal day",
+2: "Normal day",
+3: "Normal day",
+4: "Normal day",
+5: "Normal day",
+6: "Normal day",
+7: "Normal day",
+8: "Normal day",
+9: "Normal day",
+10: "Normal day",
+11: "Normal day",
+12: "Normal day",
+13: "Normal day",
+14: "Normal day",
+15: "Food15",
+16: "No school, No lunch service",
+17: "Food17",
+18: "Food18",
+19: "Advisory. Single assembly schedule.",
+20: "No school, No lunch service",
+21: "No school, No lunch service",
+22: "Normal day",
+23: "Normal day",
+24: "Normal day",
+25: "Advisory. Double assembly schedule.",
+26: "Normal day",
+27: "Normal day",
+28: "Normal day",
+29: "Advisory. Double assembly schedule.",
 };
 
-var weekday = {
-  0: "Sunday",
-  1: "Monday",
-  2: "Tuesday",
-  3: "Wednesday",
-  4: "Thursday",
-  5: "Friday",
-  6: "Saturday",
+var bSchedule2 = {
+1: "Normal day",
+2: "Normal day",
+3: "Normal day",
+4: "Normal day",
+5: "Normal day",
+6: "Normal day",
+7: "Normal day",
+8: "Normal day",
+9: "Normal day",
+10: "Normal day",
+11: "Normal day",
+12: "Normal day",
+13: "Normal day",
+14: "Normal day",
+15: "Food15",
+16: "No school, No lunch service",
+17: "Food17",
+18: "Food18",
+19: "Advisory. Single assembly schedule.",
+20: "No school, No lunch service",
+21: "No school, No lunch service",
+22: "Normal day",
+23: "Normal day",
+24: "Normal day",
+25: "Advisory. Double assembly schedule.",
 };
 
 //Based on the reported day of the clients computer, the food in the array will be displayed.//
-function lunchDay() {
-    var d = new Date();
-    var n = d.getDate();
-    var w = d.getDay();
-    document.getElementById('demo').innerHTML = lunch[n] + weekday[w];
+function bellSchedule() {
+    var d1 = new Date();
+    var n1 = d1.getDate();
+    var m1 = d1.getMonth();
+    var nextMonth = m1 + 1;
+    document.getElementById('showSchedule').innerHTML = bSchedule[n1];
+    if (n1 > DaysInMonth[m1]) {
+    		document.getElementById('showSchedule2').innerHTML = bSchedule[n1+1];
+    }
+    else {
+    		document.getElementById('showSchedule2').innerHTML = bSchedule2[n1-nextMonth+1];
+    }
 }
+
+bellSchedule();
